f7950bae0899719354cc142c0d40e4267d5f9e50
Glitch (hello-express) <none>
2020-07-30 07:41:36 +0000

    🏃👋 Checkpoint ./server.js:20086806/724

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var app = express();
+const express = require('express'); 
+const app = express(); 
+ 
 
-
-
-
-app.get('/todo',function(request, response){
-    response.send(`
-<ul>
-  <li> asd</li>
-`)
+app.get('/', function(req, res){
+    res.send("Hello World");
 })
+app.set('views', './views'); // Thư mục views nằm cùng cấp với file app.js
+app.set('view engine', 'pug'); // Sử dụng pug làm view engine
+app.listen(process.env.PORT, () => {
+  console.log("Server listening on port " + process.env.PORT);
+});
