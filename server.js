09c9cc7f34a3b97f44d6dfa7171cd8a0343a958f
Glitch (hello-express) <none>
2020-07-30 07:30:12 +0000

    🚞🐜 Checkpoint ./server.js:20086806/218 ./views/index.html:20086806/1

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine','pug')
-app.set('views','./views')
 
 
-app.get('/',function(request, response){
-    response.render('index',{
-        users: [
-            {id: 1, name: 'Tai'},
-            {id: 2, name: 'tuan anh'}
 
-        ]
-    })
+app.get('/todo',function(request, response){
+    response.send(`
+<ul>
+  <li> asd</li>
+`)
 })
