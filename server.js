fb1bea8fe60d74736bca4feac56478382db2aebb
Glitch (hello-express) <none>
2020-07-30 07:18:34 +0000

    🔔👕 Checkpoint ./server.js:20086806/1709 ./index.pug:20086806/217

--- server.js
+++ server.js
@@ -1,34 +1,16 @@
-// server.js
-// where your node app starts
+var express = require('express');
+var app = express();
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+app.set('view engine','pug')
+app.set('views','./views')
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.get('/',function(request, response){
+    response.render('index',{
+        users: [
+            {id: 1, name: 'Tai'},
+            {id: 2, name: 'tuan anh'}
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+        ]
+    })
+})
