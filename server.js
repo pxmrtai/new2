6c57e0a6e4b90a8fb83b03ff3280d5ea502305fc
Glitch (hello-express) <none>
2020-08-05 03:06:55 +0000

    🌕👦 Checkpoint ./views/todos/index.pug:20086806/35 ./server.js:20086806/419

--- server.js
+++ server.js
@@ -3,32 +3,28 @@ const app = express();
 app.set('views', './views'); // Thư mục views nằm cùng cấp với file app.js
 app.set('view engine', 'pug'); // Sử dụng pug làm view engine
 
-var todo =[
+var todos =[
             {name: 'đi chợ'},
             {name: 'nấu cơm'},
             {name: 'rửa bát'},
             {name: 'học coderX'}
-          
-
+        
         ]
-var nam =[
-            
-            {name: 'nấu cơm'},
-            {name: 'rửa bát'},
-            {name: 'học coderX'}
-          
 
-        ]
-app.get('/todos',function(request, response){
-    response.render('todos/index',{
-        todos: todo
+        
+app.get('/todos',function(req, res){
+    res.render('todos/index',{
+        todos: todos
     })
 })
 app.get('/todos',function(req,res){
-   res.render('todos/index',{
-     nam:nam
-   })
+  var q = req.query.q;
+  var matched = todos.filter(function(todo){
+    return todo.name.indexOf(q) !== -1; 
+  })
+  a
 })
+
 
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
